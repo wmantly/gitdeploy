260881acd7e73f19713b48c336b1a2987c6d04fb
William Mantly <william@718it.biz>
2016-06-16 18:49:32 -0400

    stuff

--- routes/create.sh
+++ routes/create.sh
@@ -4,10 +4,10 @@ name="$1"
 sshURL="$2"
 
 eval "$(ssh-agent -s)"
-ssh-add /root/.ssh/github_rsa
-cd /var/www/gitwrapper
-mkdir $name
-cd $name
+ssh-add /root/.ssh/id_github_rsa
+mkdir /var/www/gitwrapper/$name
+cd /var/www/gitwrapper/$name
+echo pwd
 
 git clone $sshURL .
 git checkout $name
@@ -25,12 +25,12 @@ python3 manage.py migrate
 chmod 777 .
 chmod 777 db.sqlite3
 
-echo '<VirtualHost *:80>' > /etc/apache2/sites-enabled/runner.conf
-echo '    Alias /static /var/www/gitwrapper/$name/staticfiles' > /etc/apache2/sites-enabled/runner.conf
-echo '    WSGIDaemonProcess $name python-path=/var/www/gitwrapper/$name:/var/www/gitwrapper/$name/env/lib/python3.4/site-packages' >> /etc/apache2/sites-enabled/runner.conf
-echo '    WSGIProcessGroup $name' >> /etc/apache2/sites-enabled/runner.conf
-echo '    WSGIScriptAlias / /var/www/gitwrapper/$name/project/wsgi.py' >> /etc/apache2/sites-enabled/runner.conf
-echo '</VirtualHost>' >> /etc/apache2/sites-enabled/runner.conf
+echo "<VirtualHost *:80>" > /etc/apache2/sites-enabled/$name.conf
+echo "    Alias /static /var/www/gitwrapper/$name/staticfiles" > /etc/apache2/sites-enabled/$name.conf
+echo "    WSGIDaemonProcess $name python-path=/var/www/gitwrapper/$name:/var/www/gitwrapper/$name/env/lib/python3.4/site-packages" >> /etc/apache2/sites-enabled/$name.conf
+echo "    WSGIProcessGroup $name" >> /etc/apache2/sites-enabled/$name.conf
+echo "    WSGIScriptAlias / /var/www/gitwrapper/$name/project/wsgi.py" >> /etc/apache2/sites-enabled/$name.conf
+echo "</VirtualHost>" >> /etc/apache2/sites-enabled/$name.conf
 
 
 service apache2 restart
