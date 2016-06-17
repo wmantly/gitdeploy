f2eaa293395155817d1d3f83c182045d483f0a30
William Mantly <william@718it.biz>
2016-06-16 21:00:05 -0400

    stuff

--- routes/create.sh
+++ routes/create.sh
@@ -19,8 +19,9 @@ pip install -r requirements.txt
 cp /var/www/local_settings.py project/settings/local_settings.py
 echo "BRANCH = '$name'" >> project/settings/local_settings.py
 
+python manage.py createcachetable
 python3 manage.py migrate
-python3 manage.py loaddata "/var/www/django.dump"
+python3 manage.py loaddata "/var/www/django"
 
 git checkout $name
 
