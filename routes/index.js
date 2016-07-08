c9bf7c40915fb8f396bec7de298b2b5f2996bc3d
root <wmantly@gmail.com>
2016-07-08 16:26:32 -0400

    old way

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var exec = require('child_process').exec;
 /* GET home page. */
 router.all('/', function(req, res, next) {
+    console.log("some one is here");
     if(req.headers['x-github-event'] === 'push'){
+	console.log("got a push!");
         if(req.body.ref === "refs/heads/master"){
             console.log('time to update master!');
             exec('/var/www/gitwrapperdeploy.sh', console.log, console.log);
