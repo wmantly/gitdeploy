e0963c1c299ef2cf9c3d02de964b3f86baa20f44
william mantly <wmantly@gmail.com>
2016-10-01 00:16:40 -0400

    node

--- create.sh
+++ create.sh
@@ -7,13 +7,18 @@ echo "starting $sshURL on $name"
 
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/id_github_rsa
-mkdir /var/www/gitwrapper/$name
-cd /var/www/gitwrapper/$name
+mkdir $workingPath
+cd $workingPath
 chmod 777 .
 echo `pwd`
 
+workingPath = /var/www/gitwrapper/$name
+nodePort = `./random_port.py`
 DJANGO_SETTINGS_MODULE=project.settings.prod
 export DJANGO_SETTINGS_MODULE=project.settings.prod
+
+NODE_ENV = 'staging'
+export NODE_ENV = 'staging'
 
 git clone $sshURL .
 
@@ -23,6 +28,7 @@ source env/bin/activate
 
 cp /var/www/local_settings.py project/settings/local_settings.py
 echo "BRANCH = '$name'" >> project/settings/local_settings.py
+echo "NODEPORT = '$name'" >> project/settings/local_settings.py
 
 echo "checking out to prod for set up"
 git checkout prod
@@ -40,14 +46,22 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 chmod 777 db.sqlite3
 
+echo "starting node add"
+forever stop $workingPath/node_rtc/app.js
+forever start $workingPath/node_rtc/app.js
+
 echo "creating apache VirtualHost file"
 
 echo "<VirtualHost *:80>" > /etc/apache2/sites-enabled/$name.conf
 echo "    ServerName $name.staging.bytedev.co" >> /etc/apache2/sites-enabled/$name.conf
-echo "    Alias /static /var/www/gitwrapper/$name/staticfiles" >> /etc/apache2/sites-enabled/$name.conf
-echo "    WSGIDaemonProcess $name python-path=/var/www/gitwrapper/$name:/var/www/gitwrapper/$name/env:/var/www/gitwrapper/$name/env/lib/python3.5/site-packages" >> /etc/apache2/sites-enabled/$name.conf
+echo "    Alias /static $workingPath/staticfiles" >> /etc/apache2/sites-enabled/$name.conf
+echo "    WSGIDaemonProcess $name python-path=$workingPath:$workingPath/env:$workingPath/env/lib/python3.5/site-packages" >> /etc/apache2/sites-enabled/$name.conf
 echo "    WSGIProcessGroup $name" >> /etc/apache2/sites-enabled/$name.conf
-echo "    WSGIScriptAlias / /var/www/gitwrapper/$name/project/wsgi.py" >> /etc/apache2/sites-enabled/$name.conf
+echo "    WSGIScriptAlias / $workingPath/project/wsgi.py" >> /etc/apache2/sites-enabled/$name.conf
+echo "    <Location '/socket.io'>" >> /etc/apache2/sites-enabled/$name.conf
+echo "        ProxyPass http://localhost:$nodePort/V1" >> /etc/apache2/sites-enabled/$name.conf
+echo "        ProxyPassReverse http://localhost:$nodePort/V1" >> /etc/apache2/sites-enabled/$name.conf
+echo "    </Location>" >> /etc/apache2/sites-enabled/$name.conf
 echo "</VirtualHost>" >> /etc/apache2/sites-enabled/$name.conf
 
 /usr/sbin/service apache2 restart
--- update.sh
+++ update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 name="$1"
 sshURL="$2"
+workingPath = /var/www/gitwrapper/$name
+nodePort = `./random_port.py`
 
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/id_github_rsa
+
+
 DJANGO_SETTINGS_MODULE=project.settings.prod
 export DJANGO_SETTINGS_MODULE=project.settings.prod
-cd /var/www/gitwrapper/$name
+cd $workingPath
 
 source env/bin/activate
 
@@ -14,12 +18,19 @@ git stash
 git pull --force origin $name
 
 ./scripts/setup.sh
+source env/bin/activate
 
-python3 manage.py collectstatic --noinput
+# python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 
 chmod 777 .
 chmod 777 db.sqlite3
+
+echo "starting node app"
+NODE_ENV = 'staging'
+export NODE_ENV = 'staging'
+forever stop $workingPath/node_rtc/app.js
+forever start $workingPath/node_rtc/app.js
 
 /usr/sbin/service apache2 restart
 
