ae48b9e7159287b9b026c22f9a4a64f5b2f2a133
william mantly <wmantly@gmail.com>
2016-10-11 22:29:56 -0400

    stop node stuff

--- delete.sh
+++ delete.sh
@@ -2,8 +2,13 @@
 name="$1"
 sshURL="$2"
 
+nodePort=`cat env/nodePort`
+export NODEPORT=$nodePort
+forever stop $workingPath/node_rtc/app.js
+
 rm -rf /var/www/gitwrapper/$name
 rm /etc/apache2/sites-enabled/$name.conf
+
 
 /usr/sbin/service apache2 restart
 
